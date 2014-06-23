0ed52c5c0605935943980be4b26ae4fd791b00cd
marcelo-ot <marcelob@onetree.com>
2014-06-23 11:06:09 -0300

    integration tests call

--- src/Callfire-csharp-sdk.IntegrationTests/CallfireCallClientTest.cs
+++ src/Callfire-csharp-sdk.IntegrationTests/CallfireCallClientTest.cs
@@ -1,7 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
 using CallFire_csharp_sdk.API;
+using CallFire_csharp_sdk.API.Rest.Clients;
+using CallFire_csharp_sdk.API.Soap;
 using CallFire_csharp_sdk.Common.DataManagement;
 using CallFire_csharp_sdk.Common.Resource;
+using CallFire_csharp_sdk.Common.Resource.Mappers;
 using NUnit.Framework;
 
 namespace Callfire_csharp_sdk.IntegrationTests
@@ -15,103 +21,234 @@ namespace Callfire_csharp_sdk.IntegrationTests
         protected CfActionQuery ActionQuery;
         protected CfQuery QuerySoundMeta;
 
-        //SendCall
+        protected const string VerifyFromNumber = "+19196991764";
+        protected const string VerifyShortCode = "67076";
+
+        public void AssertClientException<TRest, TSoap>(TestDelegate test)
+            where TRest : Exception
+            where TSoap : Exception
+        {
+            if (Client.GetType() == typeof(RestCallClient))
+            {
+                Assert.Throws<TRest>(test);
+            }
+            else
+            {
+                Assert.Throws<TSoap>(test);
+            }
+        }
+
+        /// <summary>
+        /// SendCall
+        /// </summary>
+        [Test]
+        [Ignore]
+        public void Test_SendCall()
+        {
+            var id = Client.SendCall(SendCall);
+            Assert.IsNotNull(id);
+        }
+
         [Test]
         public void Test_SendCallEmpty()
         {
-            //No To
+            AssertClientException<WebException, FaultException>(() => Client.SendCall(new CfSendCall()));
         }
         [Test]
-        public void Test_SendCallMandatoryVoice()
+        public void Test_SendCallMandatoryVoice() // TODO
         {
-            //To= valid, 2 numbers
-            //all mandatory field complete
-
+            CfToNumber[] toNumberList = { new CfToNumber { Value = VerifyFromNumber, ClientData = "Client1" } };
+            var sendCall = new CfSendCall
+            {
+                Type = CfBroadcastType.Voice,
+                ToNumber = toNumberList,
+                Item = new CfVoiceBroadcastConfig
+                {
+                    FromNumber = VerifyFromNumber,
+                    Item = "TTS: eeee",
+                    Item1 = "TTS: eeee"
+                }
+            };
+            var id = Client.SendCall(sendCall);
+            Assert.IsNotNull(id);
         }
+
         [Test]
-        public void Test_SendCallVoiceComplete()
+        public void Test_SendCallVoiceComplete() // TODO
         {
-            //To= valid
-            //all field complete
-            //ScrubBroadcastDuplicates=true
-
+            CfToNumber[] toNumberList = { new CfToNumber { Value = VerifyFromNumber, ClientData = "Client1" } };
+            var sendCall = new CfSendCall
+            {
+                Type = CfBroadcastType.Voice,
+                ToNumber = toNumberList,
+                ScrubBroadcastDuplicates = true,
+                Item = new CfVoiceBroadcastConfig
+                {
+                    FromNumber = VerifyFromNumber,
+                    Item = "TTS: eeee",
+                    MachineSoundTextVoice = "SPANISH1",
+                    Item1 = "TTS: eeee",
+                    LocalTimeZoneRestriction = new CfLocalTimeZoneRestriction
+                    {
+                        BeginTime = new DateTime(2014, 01, 01, 09, 00, 00),
+                        EndTime = new DateTime(2014, 01, 01, 17, 00, 00)
+                    },
+                    RetryConfig = new CfBroadcastConfigRetryConfig
+                    {
+                        MaxAttempts = 2,
+                        MinutesBetweenAttempts = 5,
+                        RetryPhoneTypes = new[] { CfRetryPhoneType.HomePhone },
+                        RetryResults = new[] { CfResult.NoAns }
+                    }
+                }
+            };
+            var id = Client.SendCall(sendCall);
+            Assert.IsNotNull(id);
         }
+
         [Test]
-        public void Test_SendCallMandatoryText()
+        public void Test_SendCallMandatoryFaild() 
         {
-            //To= valid, 2 numbers
-            //all mandatory field complete
-
+            CfToNumber[] toNumberList = { new CfToNumber { Value = VerifyFromNumber, ClientData = "Client1" } };
+            var sendCall = new CfSendCall
+            {
+                Type = CfBroadcastType.Text,
+                ToNumber = toNumberList,
+                Item = new CfTextBroadcastConfig()
+            };
+            AssertClientException<WebException, CommunicationException>(() => Client.SendCall(sendCall));
         }
+
         [Test]
-        public void Test_SendCallTextComplete()
+        public void Test_SendCallMandatoryIVR() //TODO
         {
-            //To= valid
-            //all field complete
-            //ScrubBroadcastDuplicates=true
-
+            CfToNumber[] toNumberList = { new CfToNumber { Value = VerifyFromNumber, ClientData = "Client1" } };
+            var sendCall = new CfSendCall
+            {
+                Type = CfBroadcastType.Voice,
+                ToNumber = toNumberList,
+                Item = new CfIvrBroadcastConfig
+                {
+                    DialplanXml = "<dialplan><play type=\"tts\">Congratulations! You have successfully configured a CallFire I V R.</play></dialplan>"
+                }
+            };
+            var id = Client.SendCall(sendCall);
+            Assert.IsNotNull(id);
         }
+
         [Test]
-        public void Test_SendCallMandatoryIVR()
+        public void Test_SendCallIVRComplete() //TODO
         {
-            //To= valid, 2 numbers
-            //all mandatory field complete
-
+            CfToNumber[] toNumberList = { new CfToNumber { Value = VerifyFromNumber, ClientData = "Client1" } };
+            var sendCall = new CfSendCall
+            {
+                Type = CfBroadcastType.Voice,
+                ToNumber = toNumberList,
+                ScrubBroadcastDuplicates = true,
+                Item = new CfIvrBroadcastConfig
+                {
+                    FromNumber = VerifyFromNumber,
+                    DialplanXml = "<dialplan><play type=\"tts\">Congratulations! You have successfully configured a CallFire I V R.</play></dialplan>",
+                    LocalTimeZoneRestriction = new CfLocalTimeZoneRestriction
+                    {
+                        BeginTime = new DateTime(2014, 01, 01, 09, 00, 00),
+                        EndTime = new DateTime(2014, 01, 01, 17, 00, 00)
+                    },
+                    RetryConfig = new CfBroadcastConfigRetryConfig
+                    {
+                        MaxAttempts = 2,
+                        MinutesBetweenAttempts = 5,
+                        RetryPhoneTypes = new[] { CfRetryPhoneType.HomePhone },
+                        RetryResults = new[] { CfResult.NoAns }
+                    }
+                }
+            };
+            var id = Client.SendCall(sendCall);
+            Assert.IsNotNull(id);
         }
+
+        /// <summary>
+        /// QueryCalls
+        /// </summary>
         [Test]
-        public void Test_SendCallIVRComplete()
+        public void Test_QueryCalls()
         {
-            //To= valid
-            //all field complete
-            //ScrubBroadcastDuplicates=true
-
-
+            var calls = Client.QueryCalls(ActionQuery);
+            Assert.IsNotNull(calls);
+            Assert.IsNotNull(calls.Calls);
+            Assert.IsTrue(calls.Calls.Any(c => c.FromNumber.Equals("12132609784")));
+            Assert.IsTrue(calls.Calls.Any(c => c.CallRecord.Any(r => r.Id.Equals(125746517001))));
         }
-
-        //QueryCalls
-
+        
         [Test]
         public void Test_QueryCallsNotExistNumber()
         {
-            //Number= not exist
+            var actionQuery = new CfActionQuery
+            {
+                ToNumber = "48945317921"
+            };
+            AssertClientException<WebException, FaultException<ServiceFaultInfo>>(() => Client.QueryCalls(actionQuery));
         }
 
         [Test]
         public void Test_QueryCallsAllResults()
         {
-
-            //go to Try it out!
-
+            var callQueryResult = Client.QueryCalls(new CfActionQuery());
+            Assert.IsNotNull(callQueryResult);
         }
 
         [Test]
         public void Test_QueryCallsComplete()
         {
-
-            //Id Valid
-            //MaxResults 20
-            //FirstResult 2
-            //Inbound=true
-            //all fields complete
-
+            var actionQuery = new CfActionQuery
+            {
+                BroadcastId = 1955902001,
+                ToNumber = VerifyShortCode,
+                MaxResults = 20,
+                FirstResult = 2,
+                Inbound = false,
+                State = new [] {CfActionState.Invalid},
+                BatchId = 1203601001,
+                FromNumber = VerifyFromNumber,
+            };
+            var callQueryResult = Client.QueryCalls(actionQuery);
+            Assert.IsNotNull(callQueryResult);
         }
+
+        [Test]
         public void Test_QueryCallsInvalidBatchId()
         {
-
-            ////BatchId
+            var actionQuery = new CfActionQuery
+            {
+                BatchId = 1203601000,
+            };
+            var callQueryResult = Client.QueryCalls(actionQuery);
+            Assert.IsNotNull(callQueryResult);
+            Assert.AreEqual(0, callQueryResult.TotalResults);
         }
 
-        //GetCall
+        /// <summary>
+        /// GetCall
+        /// </summary>
+        [Test]
+        public void Test_GetCall()
+        {
+            var call = Client.GetCall(209720137001);
+            Assert.IsNotNull(call);
+            Assert.AreEqual(call.ContactId, 165251012001);
+        }
+        
         [Test]
         public void Test_GetCallValidId()
         {
-            //ID Valido
+            var call = Client.GetCall(225924967001);
+            Assert.IsNotNull(call);
         }
 
         [Test]
         public void Test_GetCallInValidIdLetters()
         {
-            //ID InValido= "ABC"
+            AssertClientException<WebException, FaultException<ServiceFaultInfo>>(() => Client.GetCall(48944));
         }
 
         //CreateSound
@@ -120,11 +257,13 @@ namespace Callfire_csharp_sdk.IntegrationTests
         {
             //Data= file .png x example
         }
+
         [Test]
         public void Test_CreateSoundMandatoryFields()
         {
             //all mandatory fields complete
         }
+
         [Test]
         public void Test_CreateSoundNoMandatoryFields()
         {
@@ -137,35 +276,9 @@ namespace Callfire_csharp_sdk.IntegrationTests
         
 
 
+        
 
-
-
-
-
-
-
-
-
-
-
-
-        [Test]
-        [Ignore]
-        public void Test_SendCall()
-        {
-            var id = Client.SendCall(SendCall);
-            Assert.IsNotNull(id);
-        }
-
-        [Test]
-        public void Test_QueryCalls()
-        {
-            var calls = Client.QueryCalls(ActionQuery);
-            Assert.IsNotNull(calls);
-            Assert.IsNotNull(calls.Calls);
-            Assert.IsTrue(calls.Calls.Any(c => c.FromNumber.Equals("12132609784")));
-            Assert.IsTrue(calls.Calls.Any(c => c.CallRecord.Any(r => r.Id.Equals(125746517001))));
-        }
+        
 
         [Test]
         public void Test_QuerySoundMeta()
@@ -184,13 +297,7 @@ namespace Callfire_csharp_sdk.IntegrationTests
             Assert.AreEqual(soundMeta.Status, CfSoundStatus.Active);
         }
 
-        [Test]
-        public void Test_GetCall()
-        {
-            var call = Client.GetCall(209720137001);
-            Assert.IsNotNull(call);
-            Assert.AreEqual(call.ContactId, 165251012001);
-        }
+        
 
     }
 }
